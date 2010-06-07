b525d04ef3fb63a6ffcb7bfdfefa7d47bbb7a988
slavomir.hustaty@gmail.com <slavomir.hustaty@gmail.com@d9cd739a-d716-a736-fc61-626ca4d20b98>
2010-06-07 14:35:23 +0000

    unstable commit - needs to be tested on real device

--- trunk/BTHF PowerSave/src/com/hustaty/android/bluetooth/MyPhoneStateListener.java
+++ trunk/BTHF PowerSave/src/com/hustaty/android/bluetooth/MyPhoneStateListener.java
@@ -1,12 +1,21 @@
 package com.hustaty.android.bluetooth;
 
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
 public class MyPhoneStateListener extends PhoneStateListener {
 
+	private static final String BT_SERVICE_UUID = "00001101-0000-1000-8000-00805F9B34FB";
+	
 	// logger entry
 	private final static String LOG_TAG = MyPhoneStateListener.class.getName();
 
@@ -41,7 +50,9 @@ public class MyPhoneStateListener extends PhoneStateListener {
 					stateString = "Ringing";
 					if (!bluetoothAdapter.isEnabled()) {
 						bluetoothAdapter.enable();
-						//connectToDevice();
+						//FIXME small hack - to ensure phone's BT module is already active  
+						waitNMillis(1200);
+						connectToDevice();
 					}
 					break;
 			}
@@ -51,4 +62,44 @@ public class MyPhoneStateListener extends PhoneStateListener {
 		super.onCallStateChanged(state, incomingNumber);
 	}
 	
+	/**
+	 * perform connect to device - don't rely on platform anymore
+	 */
+	private void connectToDevice() {
+		if(bluetoothAdapter.isDiscovering()) {
+			bluetoothAdapter.cancelDiscovery();
+		}
+		Set<BluetoothDevice> btDevices = bluetoothAdapter.getBondedDevices();
+		for(BluetoothDevice btDevice : btDevices) {
+			if(btDevice.getBluetoothClass().getDeviceClass() == BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET) {
+				BluetoothSocket btSocket = null;
+				try {
+					Log.d(LOG_TAG, "Trying to connect to: " + btDevice.getName());
+					bluetoothAdapter.getRemoteDevice(btDevice.getAddress());
+					btSocket = btDevice.createRfcommSocketToServiceRecord(UUID.fromString(BT_SERVICE_UUID));
+					btSocket.connect();
+					//successfully connected to remote device
+					return;
+				} catch(IOException e) {
+					Log.e(LOG_TAG, "Message: " + e.getMessage() + "\nCaused by: " + e.getCause().getMessage());
+					try {
+						btSocket.close();
+					} catch (IOException ex) {
+						Log.e(LOG_TAG, "Couldn't close BluetoothSocket.");
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * to create Lag 
+	 * needs to be fixed
+	 * @param n
+	 */
+	private void waitNMillis(long n) {
+		long t = System.currentTimeMillis();
+		while(n > System.currentTimeMillis() - t) {
+		}
+	}
 }
