6940eb5245fe134d0ebbe14cba5e9abe9dcdbd96
slavomir.hustaty@gmail.com <slavomir.hustaty@gmail.com@d9cd739a-d716-a736-fc61-626ca4d20b98>
2010-07-28 10:04:52 +0000

    disabling Thread for checking bonded BT devices

--- BTHF PowerSave/src/com/hustaty/android/bluetooth/MyPhoneStateListener.java
+++ BTHF PowerSave/src/com/hustaty/android/bluetooth/MyPhoneStateListener.java
@@ -1,13 +1,8 @@
 package com.hustaty.android.bluetooth;
 
-import java.io.IOException;
-import java.util.Set;
-import java.util.UUID;
-
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothClass;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
+import android.content.SharedPreferences;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -32,6 +27,9 @@ public class MyPhoneStateListener extends PhoneStateListener {
 	@Override
 	public void onCallStateChanged(int state, String incomingNumber) {
 
+		SharedPreferences settings = service.getSharedPreferences(WidgetConfigure.PREFS_NAME, Activity.MODE_PRIVATE);
+		WidgetConfigurationHolder.loadPreferences(settings);
+		
 		bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 				
 		if(bluetoothAdapter == null) {
@@ -63,108 +61,17 @@ public class MyPhoneStateListener extends PhoneStateListener {
 					if (!bluetoothAdapter.isEnabled()) {
 						bluetoothAdapter.enable();
 					}
-//					try {  
-//                        waitUntilBluetoothAdapterIsInState(BluetoothAdapter.STATE_ON);
-//	                } catch (Exception e) {
-//                        Log.e(LOG_TAG, e.getMessage());
-//	                }
-	                //connectToDevice();
-
 					break;
 			}
 
-			if(Log.isLoggable(LOG_TAG, Log.DEBUG)) {
-				Log.d(LOG_TAG, String.format("\nonCallStateChanged: %s , %s", stateString, incomingNumber));
-			}
+			Log.d(LOG_TAG, String.format("\nonCallStateChanged: %s , %s", stateString, incomingNumber));
 			
 		}
 		
-		if(Log.isLoggable(LOG_TAG, Log.DEBUG)) {
-			Log.d(LOG_TAG, "Incoming call from:" + incomingNumber);
-		}
+		Log.d(LOG_TAG, "Incoming call from:" + incomingNumber);
 		
 		super.onCallStateChanged(state, incomingNumber);
 	}
-	
-//    /**
-//     * perform connect to device - don't rely on platform anymore
-//     */
-//    private void connectToDevice() {
-//            if(bluetoothAdapter.isDiscovering()) {
-//                    bluetoothAdapter.cancelDiscovery();
-//            }
-//            Set<BluetoothDevice> btDevices = bluetoothAdapter.getBondedDevices();
-//            for(BluetoothDevice btDevice : btDevices) {
-//                    if(btDevice.getBluetoothClass().getDeviceClass() == BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET) {
-//                            
-//                    		BluetoothSocket btSocket = null;
-//                            
-//                            try {
-//                                    Log.d(LOG_TAG, "Trying to connect to: " + btDevice.getName());
-//                                    bluetoothAdapter.getRemoteDevice(btDevice.getAddress());
-//                                    btSocket = btDevice.createRfcommSocketToServiceRecord(UUID.fromString(BT_SERVICE_UUID));
-//                                    btSocket.connect();
-//                                    //successfully connected to remote device
-//                                    return;
-//                            } catch(IOException e) {
-//                                    Log.e(LOG_TAG, "Message: " + e.getMessage() + "\nCaused by: " + e.getCause().getMessage());
-//                                    try {
-//                                            btSocket.close();
-//                                    } catch (IOException ex) {
-//                                            Log.e(LOG_TAG, "Couldn't close BluetoothSocket.");
-//                                    }
-//                            }
-//                    }
-//            }
-//    }
-//
-//    /**
-//     * waits until BluetoothAdapter is in required state 
-//     * @param state
-//     * @throws Exception
-//     */
-//    private void waitUntilBluetoothAdapterIsInState(int state) throws Exception {
-//            switch(state) {
-//                    case BluetoothAdapter.STATE_OFF:
-//                            if(bluetoothAdapter.getState() == BluetoothAdapter.STATE_TURNING_OFF) {
-//                                    waitNMillis(200);
-//                                    waitUntilBluetoothAdapterIsInState(BluetoothAdapter.STATE_OFF);
-//                            } else if(bluetoothAdapter.getState() == BluetoothAdapter.STATE_OFF) {
-//                                    Log.d(LOG_TAG, "BluetoothAdapter is in state OFF");
-//                                    return;
-//                            } else {
-//                                    //ensure we're not waiting for Godot ;)
-//                                    bluetoothAdapter.disable();
-//                                    waitUntilBluetoothAdapterIsInState(BluetoothAdapter.STATE_OFF);
-//                            }
-//                            break;
-//                    case BluetoothAdapter.STATE_ON:
-//                            if(bluetoothAdapter.getState() == BluetoothAdapter.STATE_TURNING_ON) {
-//                                    waitNMillis(200);
-//                                    waitUntilBluetoothAdapterIsInState(BluetoothAdapter.STATE_ON);
-//                            } else if(bluetoothAdapter.getState() == BluetoothAdapter.STATE_ON) {
-//                                    Log.d(LOG_TAG, "BluetoothAdapter is in state ON");
-//                                    return;
-//                            } else {
-//                                    //ensure we're not waiting for Godot ;)
-//                                    bluetoothAdapter.enable();
-//                                    waitUntilBluetoothAdapterIsInState(BluetoothAdapter.STATE_ON);
-//                            }
-//                            break;
-//                    default: 
-//                            throw new Exception("You can check only final states of BluetoothAdapter(STATE_ON|STATE_OFF).");
-//            }
-//    }
-//    
-//    /**
-//     * delay N milliseconds
-//     * @param n
-//     */
-//    private void waitNMillis(long n) {
-//            long t = System.currentTimeMillis();
-//            while(n > System.currentTimeMillis() - t){}
-//    }
-    
 	
 	/**
 	 * method to check whether call is outgoing - based on incomingNumber information
@@ -179,5 +86,37 @@ public class MyPhoneStateListener extends PhoneStateListener {
 		return false;
 	}
 	
+//  /**
+//  * perform connect to device - don't rely on platform anymore
+//  */
+// private void connectToDevice() {
+//         if(bluetoothAdapter.isDiscovering()) {
+//                 bluetoothAdapter.cancelDiscovery();
+//         }
+//         Set<BluetoothDevice> btDevices = bluetoothAdapter.getBondedDevices();
+//         for(BluetoothDevice btDevice : btDevices) {
+//                 if(btDevice.getBluetoothClass().getDeviceClass() == BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET) {
+//                         
+//                 		BluetoothSocket btSocket = null;
+//                         
+//                         try {
+//                                 Log.d(LOG_TAG, "Trying to connect to: " + btDevice.getName());
+//                                 bluetoothAdapter.getRemoteDevice(btDevice.getAddress());
+//                                 btSocket = btDevice.createRfcommSocketToServiceRecord(UUID.fromString(BT_SERVICE_UUID));
+//                                 btSocket.connect();
+//                                 //successfully connected to remote device
+//                                 return;
+//                         } catch(IOException e) {
+//                                 Log.e(LOG_TAG, "Message: " + e.getMessage() + "\nCaused by: " + e.getCause().getMessage());
+//                                 try {
+//                                         btSocket.close();
+//                                 } catch (IOException ex) {
+//                                         Log.e(LOG_TAG, "Couldn't close BluetoothSocket.");
+//                                 }
+//                         }
+//                 }
+//         }
+// }
+//
 	
 }
