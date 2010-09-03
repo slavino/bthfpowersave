bac80d302f1591e26a6882f786fd8f1dc1812813
slavomir.hustaty@gmail.com <slavomir.hustaty@gmail.com@d9cd739a-d716-a736-fc61-626ca4d20b98>
2010-09-03 11:51:35 +0000

    new screenshots

--- BTHF PowerSave/gen/com/hustaty/android/bluetooth/R.java
+++ BTHF PowerSave/gen/com/hustaty/android/bluetooth/R.java
@@ -11,31 +11,40 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int off=0x7f020001;
-        public static final int on=0x7f020002;
-        public static final int on_off=0x7f020003;
-        public static final int unknown_state=0x7f020004;
+        public static final int floppy=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int off=0x7f020002;
+        public static final int on=0x7f020003;
     }
     public static final class id {
-        public static final int Base=0x7f060002;
-        public static final int imagebutton=0x7f060003;
-        public static final int processOutgoingCallsToggleButton=0x7f060001;
-        public static final int switchOffBTAfterCallEndedToggleButton=0x7f060000;
+        public static final int Base=0x7f060004;
+        public static final int config_save_button=0x7f060003;
+        public static final int imagebutton=0x7f060005;
+        public static final int processOutgoingCallsToggleButton=0x7f060002;
+        public static final int switchOffBTAfterCallEndedToggleButton=0x7f060001;
+        public static final int switchOffService=0x7f060000;
     }
     public static final class layout {
-        public static final int configure=0x7f030000;
-        public static final int widget=0x7f030001;
+        public static final int config_activity=0x7f030000;
+        public static final int configure=0x7f030001;
+        public static final int widget=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int infoText=0x7f050006;
-        public static final int keepBtOnAfterCallEnded=0x7f050004;
-        public static final int processAlsoOutgoingCalls=0x7f050005;
-        public static final int save_config=0x7f050001;
-        public static final int toastTextAfterDeletingWidget=0x7f050007;
-        public static final int toggleButton_NO=0x7f050003;
-        public static final int toggleButton_YES=0x7f050002;
+        public static final int feedbackText=0x7f05000b;
+        public static final int infoText=0x7f05000a;
+        public static final int keepBtOnAfterCallEnded=0x7f050007;
+        public static final int processAlsoOutgoingCalls=0x7f050009;
+        public static final int save_config=0x7f050002;
+        public static final int service_name=0x7f050001;
+        public static final int switchOffEntrireService=0x7f050008;
+        public static final int toastTextAfterDeletingWidget=0x7f05000c;
+        public static final int toastTextOnBTAdapterNotReachable=0x7f05000e;
+        public static final int toastTextOnBTDeviceNotPaired=0x7f05000d;
+        public static final int toggleButton_NO=0x7f050004;
+        public static final int toggleButton_OFF=0x7f050006;
+        public static final int toggleButton_ON=0x7f050005;
+        public static final int toggleButton_YES=0x7f050003;
     }
     public static final class xml {
         public static final int widget=0x7f040000;
