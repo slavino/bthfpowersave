52aa279422defe80d80c3068cec6e68cda7762dc
slavomir.hustaty@gmail.com <slavomir.hustaty@gmail.com@d9cd739a-d716-a736-fc61-626ca4d20b98>
2010-09-06 15:53:42 +0000

    added licence moved MODE_PRIVATE of SharedPreferences to MODE_WORLD_WRITEABLE

--- BTHF PowerSave/src/com/hustaty/android/bluetooth/ConfigurationActivity.java
+++ BTHF PowerSave/src/com/hustaty/android/bluetooth/ConfigurationActivity.java
@@ -1,3 +1,19 @@
+/*
+ *	This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.hustaty.android.bluetooth;
 
 import android.app.Activity;
@@ -10,6 +26,11 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ToggleButton;
 
+/**
+ * 
+ * @author Slavomir Hustaty
+ *
+ */
 public class ConfigurationActivity extends Activity {
 
 	// logger entry
@@ -20,7 +41,7 @@ public class ConfigurationActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.config_activity);
 
-		SharedPreferences settings = getSharedPreferences(WidgetConfigure.PREFS_NAME, MODE_PRIVATE);
+		SharedPreferences settings = getSharedPreferences(WidgetConfigure.PREFS_NAME, MODE_WORLD_WRITEABLE);
 	    
 		WidgetConfigurationHolder.loadPreferences(settings);
 		
@@ -40,7 +61,7 @@ public class ConfigurationActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				
-                SharedPreferences settings = getSharedPreferences(WidgetConfigure.PREFS_NAME, MODE_PRIVATE);
+                SharedPreferences settings = getSharedPreferences(WidgetConfigure.PREFS_NAME, MODE_WORLD_WRITEABLE);
                 SharedPreferences.Editor editor = settings.edit();
                 
                 //set service ON/OFF
