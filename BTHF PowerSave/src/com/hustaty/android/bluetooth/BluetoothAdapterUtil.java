a4e7848c4a9ca5dcfca6ed904c29fdcfeaa9e064
slavomir.hustaty@gmail.com <slavomir.hustaty@gmail.com@d9cd739a-d716-a736-fc61-626ca4d20b98>
2010-11-19 21:24:07 +0000

    added force BT string, commented out not working code

--- BTHF PowerSave/src/com/hustaty/android/bluetooth/BluetoothAdapterUtil.java
+++ BTHF PowerSave/src/com/hustaty/android/bluetooth/BluetoothAdapterUtil.java
@@ -17,9 +17,6 @@
 package com.hustaty.android.bluetooth;
 
 import android.bluetooth.BluetoothAdapter;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
 import android.util.Log;
 
 /**
